8af1c0e6daae55760cf9907f60f5f1ac222a742e
Guilherme Aguiar <kadeguilherme@gmail.com>
2023-04-16 18:43:27 -0300

    Add o arquivo README

---
+++ Project-1/README.md
@@ -0,0 +1,14 @@
+## hello-world.sh
+
+<details>
+  <summary>RESUMO</summary>
+
+- Aprendemos sobre echo
+- Shebang
+- Criar variavel
+- Atribuicao de variavel
+- Uso do man e help
+
+</details>
+
+
---
+++ Project-1/hello-world.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Displat 'Hello'
+echo 'Hello'
+
+# Assign a value to a variable
+WORD='script'
+
+# Display that value using the variable
+echo "$WORD"
+
+# Demostrate that single quotes cause variables to NOT get expanded.
+echo '$WORD'
+
+# Combine the variable with hard-coded text.
+echo "This is a sheall $WORD"
+
+# Display the contents of the variable using an alternative synstax.
+echo "This is a she ${WORD}"
+
+# Append text to the variable
+echo "${WORD}ing is fun!"
+
+# Show how NOT to append text to a variable.
+# THis doesn't work
+echo "$WORDing is fun!"
+
+#Create a new variable
+ENDING='ed'
+
+# Combine the two variables.
+echo "This is ${WORD}${ENDING}."
+
+# Change the value stored in the ENDING variable.
+ENDING='ing'
+echo "This is ${WORD}${ENDING}."
+
+# Reassign value to ENDING
+ENDING='s'
+echo "You are going to write many ${WORD}$ENDING in this class."
